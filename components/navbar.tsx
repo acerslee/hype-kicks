6b865a704c5c6b607c4f748343ee15aac7cebcb8
Alex Lee <ahjlee712@gmail.com>
2021-06-24 08:55:53 -0400

    dried the navbar and fixed fly menu styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -29,6 +29,27 @@ const Navbar = () => {
     })
   };
 
+  const renderMenuList = (indexOne: number, indexTwo: number) => {
+    return(
+      <ul className="px-4 w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-r lg:border-b-0 pb-6 pt-6 lg:pt-3">
+        {brands.map((brand: (string|number), index: number) => {
+          if (index >= indexOne && index < indexTwo){
+            return(
+              <li key = {index}>
+                <p
+                  className="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white"
+                  onClick = {() => handleBrandRoute(brand)}
+                >
+                  {brand}
+                </p>
+              </li>
+            )
+          }
+        })}
+      </ul>
+    )
+  };
+
   return(
     <nav className="relative bg-white border-b-2 border-gray-300 text-gray-900">
       <div className="container mx-auto flex  justify-between">
@@ -46,70 +67,10 @@ const Navbar = () => {
               <label htmlFor="toggle-one" className="block cursor-pointer py-6 px-4 lg:p-6 text-sm lg:text-base font-bold">Brands</label>
               <div role="toggle" className="p-6 mega-menu mb-16 sm:mb-0 shadow-xl bg-green-900">
                 <div className="container mx-auto w-full flex flex-wrap justify-between mx-2">
-                  <ul className="px-4 w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-r lg:border-b-0 pb-6 pt-6 lg:pt-3">
-                    {brands.map((brand: (string|number), index: number) => {
-                      while (index < 7){
-                        return(
-                          <li key = {index}>
-                            <p
-                              className="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white"
-                              onClick = {() => handleBrandRoute(brand)}
-                            >
-                              {brand}
-                            </p>
-                          </li>
-                        )
-                      }
-                    })}
-                  </ul>
-                  <ul className="px-4 w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-r lg:border-b-0 pb-6 pt-6 lg:pt-3">
-                    {brands.map((brand: (string|number), index: number) => {
-                      if (index >= 7 && index < 14 ){
-                        return(
-                          <li key = {index}>
-                            <p
-                              className="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white"
-                              onClick = {() => handleBrandRoute(brand)}
-                            >
-                              {brand}
-                            </p>
-                          </li>
-                        )
-                      }
-                    })}
-                  </ul>
-                  <ul className="px-4 w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-r lg:border-b-0 pb-6 pt-6 lg:pt-3">
-                    {brands.map((brand: (string|number), index: number) => {
-                      if (index >= 14 && index < 21 ){
-                        return(
-                          <li key = {index}>
-                            <p
-                              className="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white"
-                              onClick = {() => handleBrandRoute(brand)}
-                            >
-                              {brand}
-                            </p>
-                          </li>
-                        )
-                      }
-                    })}
-                  </ul>
-                  <ul className="px-4 w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-r lg:border-b-0 pb-6 pt-6 lg:pt-3">
-                    {brands.map((brand: (string|number), index: number) => {
-                      if (index >= 21 && index < 29 ){
-                        return(
-                          <li key = {index}>
-                            <p
-                              className="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white"
-                              onClick = {() => handleBrandRoute(brand)}
-                            >
-                              {brand}
-                            </p>
-                          </li>
-                        )
-                      }
-                    })}
-                  </ul>
+                  {renderMenuList(0, 7)}
+                  {renderMenuList(7, 14)}
+                  {renderMenuList(14, 21)}
+                  {renderMenuList(21, 28)}
                 </div>
               </div>
           </li>
