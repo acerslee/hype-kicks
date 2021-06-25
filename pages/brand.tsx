0869b46ae67dcdb1fbc2b412ddf454f865b20e8f
Alex Lee <ahjlee712@gmail.com>
2021-06-25 16:18:46 -0400

    Added prisma

--- pages/brand.tsx
+++ pages/brand.tsx
@@ -1,6 +1,6 @@
-import Navbar from '../components/navbar';
-import Footer from '../components/footer';
 import { useState, useEffect } from 'react';
+import Footer from '../components/footer';
+import Navbar from '../components/navbar';
 import axios from 'axios';
 
 interface Props{
