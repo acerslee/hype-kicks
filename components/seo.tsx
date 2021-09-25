c40f6749183a1f756340218d9412140d49b8af9d
acerslee <ahjlee712@gmail.com>
2021-09-25 16:53:08 -0400

    fixed seo and redis caching for brand

--- components/seo.tsx
+++ components/seo.tsx
@@ -17,7 +17,7 @@ const SEO: React.FC<Props> = path => {
     <Head>
       <title>{dynamicTitle(path.path)}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="See the stats of NBA players and teams."></meta>
+        <meta name="description" content="See your favorite shoes in this database for the sneak enthusiasts!"></meta>
         <meta charSet="UTF-8"></meta>
         <meta name="msapplication-TileColor" content="#1A1E22" />
         <meta name="theme-color" content="#0070f3" />
--- pages/api/shoes/[brand].ts
+++ pages/api/shoes/[brand].ts
@@ -4,19 +4,20 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { brand } = req.query;
+  const { year } = req.body;
 
   const redisClient = Redis.createClient(process.env.REDIS_URL)
   const DEFAULT_EXPIRATION = 3600
 
   let url: string;
   if (!req.body) url = `https://v1-sneakers.p.rapidapi.com/v1/sneakers?limit=100&releaseYear=2021&brand=${brand}`
-  else url = `https://v1-sneakers.p.rapidapi.com/v1/sneakers?limit=100&releaseYear=${req.body.year}&brand=${brand}`
+  else url = `https://v1-sneakers.p.rapidapi.com/v1/sneakers?limit=100&releaseYear=${year}&brand=${brand}`
 
   const headers = {
     "x-rapidapi-key": process.env.KEY
   }
 
-  redisClient.get(`brandList?brand=${brand}`, async (error, shoe) => {
+  redisClient.get(`brandList?brand=${brand}&year=${year}`, async (error, shoe) => {
     if (error) console.error(error)
     if (shoe) {
       res.send(JSON.parse(shoe))
@@ -24,7 +25,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         await axios
           .get(url, {headers})
           .then(({data}) => {
-            redisClient.setex(`brandList?brand=${brand}`, DEFAULT_EXPIRATION, JSON.stringify(data))
+            redisClient.setex(`brandList?brand=${brand}&year=${year}`, DEFAULT_EXPIRATION, JSON.stringify(data))
             res.send(data)
           })
           .catch(err => {
